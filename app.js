48066ae88463d5d2a56fbdfb2105fbbe102f4ee1
Meiko-Sama <thayna.matos.carlos@gmail.com>
2025-03-14 13:29:40 -0300

    Commit de paginas

--- app.js
+++ app.js
@@ -6,9 +6,19 @@ const PORT = 3000; // Porta TCP do servidor HTTP da aplicação
 
 const app = express();
 
+const index = "<a href = '/sobre' > Sobre </a> <a href = '/info' > Info </a>";
+
 // Método express.get necessita de dois parâmetros. Na ARROW FUNCTION, o primeiro
 app.get("/", (req, res) => {
-  res.send("Olá MEU SER");
+  res.send(index);
+});
+
+app.get("/sobre", (req, res) => {
+  res.send(`Você está na página "Sobre"`);
+});
+
+app.get("/info", (req, res) => {
+  res.send(`Você está na página "Informação"`);
 });
 
 // O app.listen() precisa ser SEMPRE ser executado por último. (app.js)
