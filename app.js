86ba8fabe90f901794d7393c8b07e5983257ce40
Meiko-Sama <thayna.matos.carlos@gmail.com>
2025-03-14 13:31:25 -0300

    Teste

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = 3000; // Porta TCP do servidor HTTP da aplicação
 
 const app = express();
 
-const index = "<a href = '/sobre' > Sobre </a> <a href = '/info' > Info </a>";
+const index = "<a href='/sobre'>Sobre</a><a href='/info'>Info</a>";
 
 // Método express.get necessita de dois parâmetros. Na ARROW FUNCTION, o primeiro
 app.get("/", (req, res) => {
