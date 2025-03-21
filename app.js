acaf84e417c97152750b52af830817c4ef0f1894
Meiko-Sama <thayna.matos.carlos@gmail.com>
2025-03-21 09:42:15 -0300

    Notas

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+/* INICIAR UM PROJETO NO NODE JS
+
+npm init - Digite na "raiz" do projeto, serve para iniciar o projeto 
+
+npm install ou npm i - são as instalações das bibliotecas do node onde ele chama de 'MODULE'
+
+O npm init cria o arquivo chamado "package.json", este arquivo é usado pelo npm/node
+para 'administrar' os módulos/bibliotecas/libs do projeto.
+
+Para usar um projeto que usa o node, é só fazer download do projeto, abrir um terminal
+ir para a porta do projeto e digitar esse comando "npm instal" sem parâmetro, neste 
+processo o npm irá localizar o arquivo e vai instalar todas as dependências do projeto. 
+Para rodar o projeto, após instalar as dependências, na raiz do projeto, digite: 
+"node (arquivo do projeto).js"
+
+--- GIT ---
+
+ git clone <link do repositorio>
+
+ Caso já exista uma pasta com o nome do repositorio e esta pasta esteja vazia, o cloneirá ser feito,
+ caso a pasta contenha algum arquivo , o 'git clone' não ira funcionar, para este caso digite:
+ "git clone <nome do repositorio> nome da pasta" onde "nome da pasta" precisa ser diferente da
+ pasta do repositorio. Por padrão o git cria a pasta com o nome do repositorio
+
+*/
+
 // Criação da biblioteca
 
 const express = require("express"); // importa livraria do EXPRESS
@@ -22,8 +48,7 @@ const index =
 const home = "Você está na página HOME <br> <a href='/'>Voltar</a>";
 const sobre = "Você está na página SOBRE  <br> <a href='/'>Voltar</a>";
 const login = "Você está na página de LOGIN <br> <a href='/'>Voltar</a>";
-const cadastro =
-  "Você está na página de CAsDASTRO  <br> <a href='/'>Voltar</a>";
+const cadastro = "Você está na página de CADASTRO  <br> <a href='/'>Voltar</a>";
 
 // Método express.get necessita de dois parâmetros. Na ARROW FUNCTION, o
 // primeiro são os dados do servidor (REQUISITION - 'req') o segundo, são os
